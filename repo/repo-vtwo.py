470a935b78334cbffc9a5ac860b4d6a14cf057d3
/999 <WhiteAirForcesWithMud@protonmail.com>
2023-05-30 18:52:09 +0100

    Create repo-vtwo.py

---
+++ repo/repo-vtwo.py
@@ -0,0 +1,89 @@
+import discord
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
+intents = discord.Intents.default()
+intents.members = True  # Enable the 'members' intent
+
+# Create a new bot instance
+bot = commands.Bot(command_prefix='@', intents=intents)
+
+# bible
+def scrape_repos(username):
+    url = f"https://github.com/{username}?tab=repositories"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        repo_list = soup.find_all("div", class_="col-10 col-lg-9 d-inline-block")
+        repos = []
+
+        for repo in repo_list:
+            repo_name = repo.find("a").get_text().strip()
+            repo_link = f"https://github.com/{username}/{repo_name}"
+            terminal_text = f"[{repo_name}]({repo_link})"
+            repos.append(terminal_text)
+
+        return repos
+    else:
+        return None
+
+# Event triggered when the bot is ready and connected to Discord
+@bot.event
+async def on_ready():
+    print(f'Bot connected as {bot.user.name}')
+
+# Event triggered when a message is received
+@bot.event
+async def on_message(message):
+    # Ignore messages sent by the bot itself to avoid infinite loops
+    if message.author == bot.user:
+        return
+
+    # Check if the bot is mentioned in the message
+    if bot.user in message.mentions:
+        # Get the mentioned users in the message
+        mentioned_users = message.mentions
+
+        # Find the bot's mention in the list
+        bot_mention = next((user for user in mentioned_users if user == bot.user), None)
+
+        # If the bot has been mentioned
+        if bot_mention is not None:
+
+            # Get the index of the bot's mention
+            bot_mention_index = mentioned_users.index(bot_mention)
+
+            # Check if there is a username mentioned after the bot's mention
+            if bot_mention_index < len(message.content.split()) - 1:
+
+                # Get the username entered after mentioning the bot
+                username = message.content.split()[bot_mention_index + 1]
+
+                # Generate repos based on the entered username
+                repos = scrape_repos(username)
+
+                # Check if repositories are found
+                if repos:
+
+                    # Create an embedded message
+                    embed = discord.Embed(title=f"GitHub Repositories for {username}")
+
+                    # Add a field for each repository
+                    for repo in repos:
+                        embed.add_field(name="Repository", value=repo, inline=False)
+
+                    # Send the embedded message
+                    await message.channel.send(embed=embed)
+                else:
+                    await message.channel.send("No repositories found for the provided username.")
+            else:
+                # No username entered after mentioning the bot
+                await message.channel.send("Please provide a GitHub username after mentioning the bot.")
+
+    # Process any other commands or events
+    await bot.process_commands(message)
+
+# Run the bot
+bot.run('YOUR_BOT_TOKEN')
